de818f4d73bf652be646b98a75bc6defa350209c
Alex Shikerun <chelseagrin52@gmail.com>
2021-07-19 20:58:56 +0300

    preparation (add sass) v.2

--- src/Components/Common/PageHeader/index.tsx
+++ src/Components/Common/PageHeader/index.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from "react";
+import style from "../PageHeader/header.module.scss";
 
 const PageHeader: FC = () => {
   return (
-    <div>
+    <div className={style.asd}>
       <h1>page header should be here !!!</h1>
     </div>
   );
