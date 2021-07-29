2e59b5f967c39097f0414008c338ac06d74bb125
Alex Shikerun <chelseagrin52@gmail.com>
2021-07-29 14:45:24 +0300

    add style in apge contact us

--- src/Components/ContactUsContent/index.tsx
+++ src/Components/ContactUsContent/index.tsx
@@ -21,51 +21,55 @@ const ContactUsContent: FC = () => {
   return (
     <>
       <PageHeader />
-      <div className={style.contact_us__header}>
-        <h1>CONTACT US</h1>
+      <div className={style.contact_us__wrapper}>
+        <div className={style.contact_us__header}>
+          <h1>CONTACT US</h1>
+        </div>
+        <div className={style.contact_us__input_btn}>
+          <input
+            type="text"
+            placeholder="Enter your question"
+            value={message}
+            onChange={onChangeMessage}
+          />
+          <button type="button" onClick={onSendMessage}>
+            Send
+          </button>
+        </div>
+        <div className={style.contact_us_logos__wrapper}>
+          <div className={style.contact_us__logos}>
+            <Image
+              src={twitter}
+              alt="alt logo"
+              width={130}
+              height={100}
+              onClick={() =>
+                (location.href = "https://twitter.com/?logout=1627486036279")
+              }
+            />
+            <Image
+              src={instagram}
+              alt="alt inst"
+              width={200}
+              height={100}
+              onClick={() =>
+                (location.href = "https://www.instagram.com/ivlevchef/")
+              }
+            />
+          </div>
+        </div>
+        <div className={style.contact_us_contact__wrapper}>
+          <div className={style.contact_us_contact}>
+            <span>phone +7 999 123 66 00</span>
+            <span>
+              <a href="https://mail.ru/"> mail mail@mail.ru</a>
+            </span>
+            <span>
+              <a href="http://localhost:3000/mainPage"> www.website.com</a>
+            </span>
+          </div>
+        </div>
       </div>
-      <div className={style.contact_us__input_btn}>
-        <input
-          type="text"
-          placeholder="Enter your question"
-          value={message}
-          onChange={onChangeMessage}
-        />
-        <button type="button" onClick={onSendMessage}>
-          Send
-        </button>
-      </div>
-      <div className={style.contact_us__logos}>
-        <Image
-          src={twitter}
-          alt="alt logo"
-          width={130}
-          height={100}
-          onClick={() =>
-            (location.href = "https://twitter.com/?logout=1627486036279")
-          }
-        />
-        <Image
-          src={instagram}
-          alt="alt inst"
-          width={200}
-          height={100}
-          onClick={() =>
-            (location.href = "https://www.instagram.com/ivlevchef/")
-          }
-        />
-      </div>
-
-      <div>
-        <span>phone +7 999 123 66 00</span>
-        <span>
-          <a href="https://mail.ru/"> mail mail@mail.ru</a>
-        </span>
-        <span>
-          <a href="http://localhost:3000/mainPage"> www.website.com</a>
-        </span>
-      </div>
-
       <PageFooter />
     </>
   );
